7288b2f246a6dffbc47fea06733d97601b346816
BelieverInGod <Stepanko919@gmail.com>
2022-09-25 23:07:21 +0300

    server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/shop-test'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
-        '/dist/shop-test/index.html'));});
-app.listen(process.env.PORT || 8080);
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8080;
+app.use(express.static(publicPath))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+    console.log('Server is up!');
+});
+
+
+// app.use(express.static(__dirname + '/dist/ShopTest'));
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname +
+//         '/dist/ShopTest/index.html'));});
+// app.listen(process.env.PORT || 8080);
