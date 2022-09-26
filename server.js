61a14141d1772821230417600be04202ce0c23de
BelieverInGod <Stepanko919@gmail.com>
2022-09-26 03:23:24 +0300

    doneWithPath

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const router = express.Router()
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 8080;
-app.use(express.static('build'))
+// app.use(express.static('build'))
+app.use(express.static('build'));
 
-router.post('/')
-
-app.listen(port, () => {
-    console.log('Server is up!');
+app.get('/*', function(req,res) {
+    res.sendFile(__dirname + '/build/index.html')
 });
 
+app.listen(process.env.PORT || 8080);
